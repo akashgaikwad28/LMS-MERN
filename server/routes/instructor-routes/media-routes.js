8f9a3249e062555b8e1e3e973a53becd0cfe26e6
Akash Gaikwad <akash.gaikwad9945@gmail.com>
2025-07-05 19:32:39 +0530

    corrected the import files

--- server/routes/instructor-routes/media-routes.js
+++ server/routes/instructor-routes/media-routes.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const multer = require("multer");
-const {
-  uploadMediaToCloudinary,
-  deleteMediaFromCloudinary,
-} = require("../../helpers/cloudinary");
+const {uploadMediaToCloudinary,deleteMediaFromCloudinary,} = require("../../helpers/cloudinary");
 
 const router = express.Router();
 
